1d38cfad8b44be498b7001d60a526cecade6d554
Matthew Tse <matt.s.tse@gmail.com>
2017-05-16 20:16:30 -0700

    Added nvidia driver stuff

--- ubuntu/elementary/README.md
+++ ubuntu/elementary/README.md
@@ -1,9 +1,4 @@
-To stop elementary from randomly crashing during Chrome usage:
-
-sudo vim /etc/default/grub
-
-Change GRUB_CMDLINE_LINUX_DEFAULT="quiet splash" 
-to GRUB_CMDLINE_LINUX_DEFAULT="quiet splash intel_idle.max_cstate=1"
-
-sudo update-grub
-sudo reboot
+# To install nvidia drivers:
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt-get update
+sudo apt-get install nvidia-381 nvidia-settings
--- ubuntu/elementary/after_login.sh
+++ ubuntu/elementary/after_login.sh
@@ -1,7 +1,8 @@
 # Set DPI to 2x
 gsettings set org.gnome.desktop.interface scaling-factor 2
 # Set scale to compensate for 2x DPI, and then set panning so mouse can expand across the screen
-xrandr --output HDMI-1 --scale 2x2 --panning 3840x2160+3840+0
+# 2x2 doesn't work b/c nvidia... https://askubuntu.com/questions/704503/scale-2x2-in-xrandr-causes-the-monitor-to-not-display-anything
+xrandr --output DP-1 --scale 1x1 --output HDMI-0 --right-of DP-1 --scale 2x2.001
 
 # Set key repeat speed
 xset r rate 250 40
