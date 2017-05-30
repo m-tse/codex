4d2bd9cac256d39cd6acc80aed1a4126714c88b0
Matthew Tse <mtse.google.com>
2017-05-30 10:18:20 -0700

    removed unused vim thing

--- ubuntu/sublime/stop_insert_on_focus_lost.py
+++
@@ -1,9 +0,0 @@
-# Drop this in ~/.config/sublime-text-3/Packages/stop_insert_on_focus_lost
-
-import sublime, sublime_plugin
-import os.path
-
-class StopInsertOnFocusLost(sublime_plugin.EventListener):
-    def on_deactivated(self, view):
-            if view.file_name() and not view.settings().get('command_mode'):
-                        view.run_command('exit_insert_mode')
