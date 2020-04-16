d55f3cb4cd983f909217469a75dfee1a6218c0fa
Matthew <matt.s.tse@gmail.com>
2020-04-16 12:17:02 -0400

    begin a brew cask portion of OS X install

---
+++ os_x/homebrew/install.sh
@@ -0,0 +1,6 @@
+# Installing homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+brew cask install visual-studio-code
+brew cask install google-backup-and-sync
+brew cask install rectangle
