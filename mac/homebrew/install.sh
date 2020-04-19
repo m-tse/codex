e347ac8ecebdede42405c9a2dfbcd37cbf7751db
Matthew <matt.s.tse@gmail.com>
2020-04-19 17:06:12 -0400

    update mac install.sh

--- mac/homebrew/install.sh
+++ mac/homebrew/install.sh
@@ -3,4 +3,9 @@
 
 brew cask install visual-studio-code
 brew cask install google-backup-and-sync
+# To get window management keyboard shortcuts
 brew cask install rectangle
+# To get F11/F12 volume adjustment
+brew cask install karabiner-elements
+# To get mouse 4/5 to behave as back/forward
+brew cask install sensiblesidebuttons
