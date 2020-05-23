8bc6f2a0416bafc8e3516cebaaedd6c00d1ae09b
m <1315979+mattse@users.noreply.github.com>
2020-05-23 10:21:26 -0400

    removed unused git/tmux directories

--- git/link_diff_bins.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo ln -s ~/Development/codex/git/diff-so-fancy /usr/local/bin/diff-so-fancy
-sudo ln -s ~/Development/codex/git/diff-highlight /usr/local/bin/diff-highlight
-
--- tmux/README.md
+++
@@ -1,1 +0,0 @@
-Use tmux as a more powerful screen to keep track of your things when you've ssh'd in.
