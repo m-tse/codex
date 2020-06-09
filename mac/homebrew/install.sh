bd7d2deab35d049b8ef93e19128312a912dbf2ec
Matthew Tse <1315979+m-tse@users.noreply.github.com>
2020-06-08 20:03:34 -0400

    Update install.sh

--- mac/homebrew/install.sh
+++ mac/homebrew/install.sh
@@ -11,8 +11,8 @@ brew cask install karabiner-elements
 brew cask install sensiblesidebuttons
 
 # Command line tools
-brew install \
-  bat \ # Fancy cat with syntax highlighting and line numbers
-  diff-so-fancy \ # Fancy git diff
-  tree \ # View directory tree structure
-  fd # Fancy fast find
+# Fancy cat with syntax highlighting and line numbers
+brew install bat
+brew install diff-so-fancy \ # Fancy git diff
+brew install tree \ # View directory tree structure
+brew install fd # Fancy fast find
