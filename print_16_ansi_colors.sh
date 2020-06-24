5b5e32ade3fc6673eb92e23cb6784853d2ee5371
Matthew Tse <1315979+m-tse@users.noreply.github.com>
2020-06-24 17:49:23 -0400

    Added a ansi color scheme printer

---
+++ print_16_ansi_colors.sh
@@ -0,0 +1,5 @@
+# Prints all 16 ANSI colors, useful for constructing color schemes for a terminal.
+print u"\u001b[30m A \u001b[31m B \u001b[32m C \u001b[33m D \u001b[0m"
+print u"\u001b[34m E \u001b[35m F \u001b[36m G \u001b[37m H \u001b[0m"
+print u"\u001b[30;1m A \u001b[31;1m B \u001b[32;1m C \u001b[33;1m D \u001b[0;1m"
+print u"\u001b[34;1m E \u001b[35;1m F \u001b[36;1m G \u001b[37;1m H \u001b[0;1m"
