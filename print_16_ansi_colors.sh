b4a1d54578af8acfed11cd8ef9e48298692371b6
Matthew Tse <1315979+m-tse@users.noreply.github.com>
2020-06-24 18:08:00 -0400

    Actually print the bold texts

--- print_16_ansi_colors.sh
+++ print_16_ansi_colors.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 printf "\u001b[30m black \u001b[31m red \u001b[32m green \u001b[33m yellow \u001b[0m\n"
-printf "\u001b[30;1m BLACK \u001b[31;1m RED \u001b[32;1m GREEN \u001b[33;1m YELLOW \u001b[0;1m\n"
 printf "\u001b[34m blue \u001b[35m purple \u001b[36m cyan \u001b[37m white \u001b[0m\n"
+printf "\u001b[30;1m BLACK \u001b[31;1m RED \u001b[32;1m GREEN \u001b[33;1m YELLOW \u001b[0;1m\n"
 printf "\u001b[34;1m BLUE \u001b[35;1m PURPLE \u001b[36;1m CYAN \u001b[37;1m WHITE \u001b[0;1m\n"
