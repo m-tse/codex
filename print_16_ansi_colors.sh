64db7b684d20e0171f8ed779061926574a14cb56
Matthew Tse <1315979+m-tse@users.noreply.github.com>
2020-06-24 18:02:20 -0400

    Fix print ansi colors script

--- print_16_ansi_colors.sh
+++ print_16_ansi_colors.sh
@@ -1,5 +1,5 @@
-# Prints all 16 ANSI colors, useful for constructing color schemes for a terminal.
-print u"\u001b[30m A \u001b[31m B \u001b[32m C \u001b[33m D \u001b[0m"
-print u"\u001b[34m E \u001b[35m F \u001b[36m G \u001b[37m H \u001b[0m"
-print u"\u001b[30;1m A \u001b[31;1m B \u001b[32;1m C \u001b[33;1m D \u001b[0;1m"
-print u"\u001b[34;1m E \u001b[35;1m F \u001b[36;1m G \u001b[37;1m H \u001b[0;1m"
+#!/bin/zsh
+printf "\u001b[30m black \u001b[31m red \u001b[32m green \u001b[33m yellow \u001b[0m\n"
+printf "\u001b[30;1m BLACK \u001b[31;1m RED \u001b[32;1m GREEN \u001b[33;1m YELLOW \u001b[0;1m\n"
+printf "\u001b[34m blue \u001b[35m purple \u001b[36m cyan \u001b[37m white \u001b[0m\n"
+printf "\u001b[34;1m BLUE \u001b[35;1m PURPLE \u001b[36;1m CYAN \u001b[37;1m WHITE \u001b[0;1m\n"
