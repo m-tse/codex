e5d0dd19dd616e4722d297c4c90edfd4f5dbeb58
m <1315979+mattse@users.noreply.github.com>
2020-07-03 10:11:16 -0400

    Rename update.sh to sync_disk_to_repo.sh

--- mac/vscode/sync_disk_to_repo.sh
+++ mac/vscode/sync_disk_to_repo.sh
@@ -1,3 +1,4 @@
+# Copies the on-disk settings and keybindings to the repo version.
 cp ~/Library/Application\ Support/Code/User/settings.json ./settings.json
 cp ~/Library/Application\ Support/Code/User/keybindings.json ./keybindings.json
 
