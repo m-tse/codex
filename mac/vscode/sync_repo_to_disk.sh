8ecf4b293f6db302e6cf89e6a9a8a14d57102e6b
m <1315979+mattse@users.noreply.github.com>
2020-07-03 10:17:23 -0400

    Add sync repo to disk script to mac vscode

---
+++ mac/vscode/sync_repo_to_disk.sh
@@ -0,0 +1,3 @@
+# Copies the repo settings and keybindings to the disk location to be used by vs code.
+cp ./settings.json ~/Library/Application\ Support/Code/User/settings.json 
+cp ./keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json 
