da6b766e676be6cd33565170cb866d734b264c4e
Matthew Tse <matt.s.tse@gmail.com>
2023-02-08 16:16:45 -0500

    Add number indexes to print_16_ansi_colors.sh

--- print_16_ansi_colors.sh
+++ print_16_ansi_colors.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-printf "\u001b[30m black \u001b[31m red \u001b[32m green \u001b[33m yellow \u001b[0m\n"
-printf "\u001b[34m blue \u001b[35m purple \u001b[36m cyan \u001b[37m white \u001b[0m\n"
-printf "\u001b[30;1m BLACK \u001b[31;1m RED \u001b[32;1m GREEN \u001b[33;1m YELLOW \u001b[0;1m\n"
-printf "\u001b[34;1m BLUE \u001b[35;1m PURPLE \u001b[36;1m CYAN \u001b[37;1m WHITE \u001b[0;1m\n"
+printf "\u001b[30m 0 black \u001b[31m 1 red \u001b[32m 2 green \u001b[33m 3 yellow \u001b[0m\n"
+printf "\u001b[34m 4 blue \u001b[35m 5 purple \u001b[36m 6 cyan \u001b[37m 7 white \u001b[0m\n"
+printf "\u001b[30;1m 8 BLACK \u001b[31;1m 9 RED \u001b[32;1m 10 GREEN \u001b[33;1m 11 YELLOW \u001b[0;1m\n"
+printf "\u001b[34;1m 12 BLUE \u001b[35;1m 13 PURPLE \u001b[36;1m 14 CYAN \u001b[37;1m 15 WHITE \u001b[0;1m\n"
