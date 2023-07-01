f3df887b6734eee28dbaf9c45f350ca094fbcfdc
Matthew Tse <matt.s.tse@gmail.com>
2023-07-01 13:10:08 -0400

    Add normal/BRIGHT prefixes to print_16_ansi_colors.sh

--- print_16_ansi_colors.sh
+++ print_16_ansi_colors.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-printf "\u001b[30m 0 black \u001b[31m 1 red \u001b[32m  2 green \u001b[33m  3 yellow \u001b[0m \u001b[34m  4 blue \u001b[35m  5 purple \u001b[36m  6 cyan \u001b[37m  7 white \u001b[0m\n"
-printf "\u001b[30;1m 8 BLACK \u001b[31;1m 9 RED \u001b[32;1m 10 GREEN \u001b[33;1m 11 YELLOW \u001b[0;1m \u001b[34;1m 12 BLUE \u001b[35;1m 13 PURPLE \u001b[36;1m 14 CYAN \u001b[37;1m 15 WHITE \u001b[0;1m\n"
+printf "normal: \u001b[30m 0 black \u001b[31m 1 red \u001b[32m  2 green \u001b[33m  3 yellow \u001b[0m \u001b[34m  4 blue \u001b[35m  5 purple \u001b[36m  6 cyan \u001b[37m  7 white \u001b[0m\n"
+printf "BRIGHT: \u001b[30;1m 8 BLACK \u001b[31;1m 9 RED \u001b[32;1m 10 GREEN \u001b[33;1m 11 YELLOW \u001b[0;1m \u001b[34;1m 12 BLUE \u001b[35;1m 13 PURPLE \u001b[36;1m 14 CYAN \u001b[37;1m 15 WHITE \u001b[0;1m\n"
