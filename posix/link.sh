40633ba0ddabf0facf8e1537518e1226c2703757
Matthew Tse <matt.s.tse@gmail.com>
2023-06-30 22:24:02 -0400

    Fix link.sh

--- posix/link.sh
+++ posix/link.sh
@@ -1,8 +1,9 @@
 # Creates homedir symlinks to the files in this directory, allowing direct git tracking!
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(dirname "$0")
 
 ln -s $SCRIPT_DIR/.profile ~/.profile
 ln -s $SCRIPT_DIR/.zshrc ~/.zshrc
 ln -s $SCRIPT_DIR/.gitconfig ~/.gitconfig
 ln -s $SCRIPT_DIR/.vimrc ~/.vimrc
-ln -s $SCRIPT_DIR/.submodules ~/.submodules
+# -h is required so we don't create ~/.submodules/.submodules
+ln -hs $SCRIPT_DIR/.submodules ~/.submodules
